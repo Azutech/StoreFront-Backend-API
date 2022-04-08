81dc863ff64eea633ea9cc7f27c79f48fa660ae5
unknown <azunna.onugha@gmail.com>
2022-04-08 15:05:32 +0100

    tsconfig.json was created

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import  express from 'express'
+import  express, {Request, Response} from 'express'
 import bodyParser from 'body-parser'
 
 
 const PORT: Number = 3000
 const app: express.Application =express()
+
+app.get('/', (req: Request, res: Response) =>{
+    console.log(`${PORT}, is the port number`)
+    res.status(200).json({message: ''})
+})
 
 
 
