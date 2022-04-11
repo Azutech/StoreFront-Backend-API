ea3c2d84d862f8783d99a1dca9ee82d53320cb1a
Azutech <azunna.onugha@gmail.com>
2022-04-11 12:35:44 +0100

    updated sql files

--- migrations/sqls/20220410151307-Order-up.sql
+++
@@ -1,5 +0,0 @@
-/* Replace with your SQL commands */
-
-CREATE TABLE (
-    
-)
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
-import  express, {Request, Response} from 'express'
-import bodyParser from 'body-parser'
+import  express, {Request, Response} from 'express';
+import bodyParser, { json } from 'body-parser';
 
 
-const PORT: Number = 3000
-const app: express.Application =express()
+const PORT: Number = 3000;
+const app: express.Application =express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) =>{
     console.log(`${PORT}, is the port number`)
     res.status(200).json({message: ''})
-})
+});
 
 
 
 app.listen(PORT, () =>{
     console.log(`The server is running at this Port ${PORT}`)
-})
+});
