04ab1873cb3e5d8fd61d067e6f6bee59d8875a79
Azutech <azunna.onugha@gmail.com>
2022-04-11 11:02:07 +0100

    added auth

---
+++ migrations/sqls/20220410151307-Order-up.sql
@@ -0,0 +1,5 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE (
+    
+)
---
+++ migrations/sqls/20220411093440-product-up.sql
@@ -0,0 +1,10 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (100),
+    price INTEGER, 
+    type VARCHAR,
+    origin VARCHAR
+
+)
