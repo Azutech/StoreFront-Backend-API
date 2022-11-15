41dd65b8a0a7bead78de20b29701f953ca6c243d
Azutech <azunna.onugha@gmail.com>
2022-11-15 14:15:20 +0100

    bug-fixes

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import bodyParser from 'body-parser';
 import routes from './routes';
 import swaggerDoc from 'swagger-ui-express';
 import swaggerDocumentations from './helpers/documentation';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 
 
@@ -10,7 +13,7 @@ import swaggerDocumentations from './helpers/documentation';
 
 
 
-const PORT: number = 3000;
+const Port: string | number = process.env.PORT || 3000;
 const app: express.Application = express();
 
 app.use('/documetations', swaggerDoc.serve)
@@ -24,13 +27,13 @@ app.use('/api', routes)
 
 
 app.get('/', (req: Request, res: Response) =>{
-    console.log(`${PORT}, is the port number`)
+    console.log(`${Port}, is the port number`)
     res.status(200).json({message: 'Yep!! Welcome to my Coffee'})
     
 });
 
 
 
-app.listen(PORT, () =>{
-    console.log(`The server is running at this Port ${PORT}`)
+app.listen(Port, () =>{
+    console.log(`The server is running at this Port ${Port}`)
 });
